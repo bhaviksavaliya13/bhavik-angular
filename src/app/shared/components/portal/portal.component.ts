498dc0c173b201c5f77da3de5345c1e20211fb90
bhaviksavaliya13 <73424152+bhaviksavaliya13@users.noreply.github.com>
2021-05-03 19:00:27 +0530

    Add files via upload

--- src/app/shared/components/portal/portal.component.ts
+++ src/app/shared/components/portal/portal.component.ts
@@ -1,6 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DEFAULT_INTERRUPTSOURCES, Idle } from '@ng-idle/core';
+import { Keepalive } from '@ng-idle/keepalive';
+import {
+  BsModalRef,
+  BsModalService,
+  ModalDirective
+} from 'ngx-bootstrap/modal';
+import { AuthService } from 'src/app/core/authentication/auth.service';
 import { MenuItemModel } from 'src/app/main/main-sidebar/menu-item/menu-item.model';
 import { appRoutingURL } from '../../configs/app-routing-url.config';
+import { appSetting } from '../../configs/app-setting.config';
 import { RoutingUrlService } from '../../services/routing-url.service';
 
 @Component({
@@ -11,12 +20,87 @@ import { RoutingUrlService } from '../../services/routing-url.service';
 })
 export class PortalComponent implements OnInit {
   menuItems: MenuItemModel[];
+  idleState = 'Not started.';
+  timedOut = false;
+  lastPing?: Date = null;
+  title = 'TimeOut Confirmation';
+  logOutButtonText = 'LogOut';
+  stayButtonText = 'Stay';
+
+  public modalRef: BsModalRef;
+
+  @ViewChild('confirmationPopup', { static: false })
+  confirmationPopup: ModalDirective;
 
   //LoaderService is for the spinner
-  constructor(private routingurlService: RoutingUrlService) {}
+  constructor(
+    private idle: Idle,
+    private keepalive: Keepalive,
+    private modalService: BsModalService,
+    private routingurlService: RoutingUrlService,
+    private authService: AuthService
+  ) {
+    // sets an idle timeout of 5 seconds, for testing purposes.
+    idle.setIdle(appSetting.SESSION.INACTIVITY_SESSION_TIMEOUT);
+    // sets a timeout period of 5 seconds. after 10 seconds of inactivity, the user will be considered timed out.
+    idle.setTimeout(appSetting.SESSION.TIMEOUT_IN_SECONDS_TIME);
+    // sets the default interrupts, in this case, things like clicks, scrolls, touches to the document
+    idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+
+    idle.onIdleEnd.subscribe(() => {
+      this.reset();
+    });
+
+    idle.onTimeout.subscribe(() => {
+      this.confirmationPopup.hide();
+      this.idleState = 'Timed out!';
+      this.timedOut = true;
+      this.authService.SignOut();
+    });
+
+    idle.onIdleStart.subscribe(() => {
+      this.idleState = "You've gone idle!";
+      this.confirmationPopup.show();
+    });
+
+    idle.onTimeoutWarning.subscribe((countdown) => {
+      this.idleState = 'You will time out in ' + countdown + ' seconds!';
+    });
+
+    // sets the ping interval to 15 seconds
+    keepalive.interval(15);
+
+    keepalive.onPing.subscribe(() => (this.lastPing = new Date()));
+
+    if (this.authService.isLoggedIn()) {
+      idle.watch();
+      this.timedOut = false;
+    } else {
+      idle.stop();
+    }
+  }
 
   ngOnInit() {
     this.createMenu();
+  }
+
+  reset() {
+    this.idle.watch();
+    this.timedOut = false;
+  }
+
+  hideConfirmationPopup(): void {
+    this.confirmationPopup.hide();
+  }
+
+  stay() {
+    this.confirmationPopup.hide();
+    this.reset();
+  }
+
+  logout() {
+    this.confirmationPopup.hide();
+    this.authService.SignOut();
   }
 
   private createMenu() {
@@ -27,6 +111,15 @@ export class PortalComponent implements OnInit {
         redirectUrl: this.routingurlService.merge(
           appRoutingURL.PORTAL_PAGE,
           appRoutingURL.DASHBOARD_PAGE
+        ),
+      },
+      {
+        title: 'Tifin Menu',
+        icon: 'nav-icon fas fa-pizza-slice',
+        redirectUrl: this.routingurlService.merge(
+          appRoutingURL.PORTAL_PAGE,
+          appRoutingURL.TIFIN_MENU_PAGE,
+          appRoutingURL.TIFIN_MENU_OVERVIEW
         ),
       },
       {
